0b2760b2fb7afc0ce07aa903aceb108ff22fbd6b
m1webste <m1webste@ucsd.edu>
2017-11-20 12:03:10 -0800

    Added no-overlap p-value to 1, bias and bias-corrected z-score functions, and comments

--- ura/create_graph.py
+++ ura/create_graph.py
@@ -14,14 +14,27 @@ def load_slowkow(filename_list=['./slowkow_databases/TRED_TF.txt',
                                 './slowkow_databases/ENCODE_TF.txt',
                                 './slowkow_databases/Neph2012_TF.txt',
                                 './slowkow_databases/TRRUST_TF.txt',
-                                './slowkow_databases/Marbach2016_TF.txt']):
+                                './slowkow_databases/Marbach2016_TF.txt'], sep = '\n'):
 
+    """
+        This function assumes you are using files that are formatted as a single list of "sep" separated
+        transcription factors. While this method is intended to load the slowkow database, any files of the
+        correct format can be processed using this method.
+
+        Args:
+            filename_list: list of strings, a list of six files from which to extract the slowkow database, or whichever files you wish
+                to extract transcription factors from
+            sep: string, the type of deliminator separating your TF entries
+
+        Returns: A list of all unique transcription factors from the lists provided
+
+    """
 
     # read files formatted as \n separated items
     return_list = []
     for file_name in filename_list:
         with open(file_name) as f:
-            lines = f.read().splitlines()
+            lines = f.read().split(sep)
             return_list.extend(lines)
 
     # convert everything to ALL CAPS
@@ -33,30 +46,58 @@ def load_slowkow(filename_list=['./slowkow_databases/TRED_TF.txt',
 
 
 
-def load_jaspar(filename):
+def load_jaspar(filename = 'jaspar_genereg_matrix.txt'):
+
+    """
+        This function loads The 2016 version of Jaspar's TF database, which can be dowloaded as a .txt file from
+        http://jaspar2016.genereg.net/. At the bottom of the page, navigate to the "Download" button. Open the
+        "database" file and download "MATRIX.txt".
+
+        Args:
+            filename = string, the path to the jaspar "MATRIX.txt" file
+
+        Returns: A list of all unique transcription factors from the file provided
+
+    """
+
     # parse jaspar file
     jasp_df = pd.read_csv(filename, sep="\t", header=None, names=['col1', 'col2', 'col3', 'col4', 'tf_genes'])
 
     # return transcription factors with ALL CAPS names
-    return list(jasp_df['tf_genes'].str.upper())
+    return list(set(jasp_df['tf_genes'].str.upper()))
 
 
 
 def create_TF_list(slowkow_bool=True,
-                       slowkow_files=['./slowkow_databases/TRED_TF.txt',
+                    slowkow_files=['./slowkow_databases/TRED_TF.txt',
                                       './slowkow_databases/ITFP_TF.txt',
                                       './slowkow_databases/ENCODE_TF.txt',
                                       './slowkow_databases/Neph2012_TF.txt',
                                       './slowkow_databases/TRRUST_TF.txt',
                                       './slowkow_databases/Marbach2016_TF.txt'],
-                       jaspar_bool=True,
-                       jaspar_file="jaspar_genereg_matrix.txt"):
+                    slowkow_sep = '\n',
+                    jaspar_bool=True,
+                    jaspar_file="jaspar_genereg_matrix.txt"):
+    """
+        This function loads The 2016 version of Jaspar's TF database, which can be dowloaded as a .txt file from
+        http://jaspar2016.genereg.net/. At the bottom of the page, navigate to the "Download" button. Open the
+        "database" file and download "MATRIX.txt".
 
+        Args:
+            slowkow_bool = boolean, whether or not to include the slowkow databases
+            slowkow_files = list or strings, a list of input file path names to the load_slowkow method
+            slowkow_sep = string, the type of deliminator used in the slowkow_files to separate the transcription factors
+            jaspar_bool = boolean, whether or not to include the jaspar database
+            jaspar_file = string, the file path where to find the jaspar database
+
+        Returns: A list of all unique transcription factors from the files provided
+
+    """
 
     TF_list = []
 
     if slowkow_bool == True:
-        slowkow_TFs = load_slowkow(slowkow_files)
+        slowkow_TFs = load_slowkow(slowkow_files, slowkow_sep)
         TF_list.extend(slowkow_TFs)
 
     if jaspar_bool == True:
@@ -66,7 +107,23 @@ def create_TF_list(slowkow_bool=True,
     return list(set(TF_list))
 
 
-def load_and_process_STRING(filename="STRING_network.xlsx"):
+def load_and_process_small_STRING(filename="STRING_network.xlsx"):
+
+    """
+        This function loads a version of the STRING database to serve as our background network. It converts the STRING
+        excel file into a pandas dataframe and extracts the edge information from that dataframe. We create two dictionaries
+        to be used when creating a networkx graph: one maps edges to their weights, and the other maps edges to their
+        weight's sign (+ or -)
+
+        Args:
+            filename = string, the path to the STRING excel file
+
+        Returns: STRING_DF = Dataframe, a pandas dataframe of the entire STRING file
+                 db_edges = list, a list of tuples of the form (source, target, edge weight)
+                 db_sign_att = list, a list of tuples of the form (source, target, sign of edge weight)
+
+    """
+
     # Load STRING database as background network
     STRING_DF = pd.read_excel(filename)
 
@@ -94,6 +151,21 @@ def load_and_process_STRING(filename="STRING_network.xlsx"):
 
 
 def filter_background(db_edges, db_sign_att, TF_list):
+
+    """
+        This function takes as input two lists (as created by our load_and_process_STRING function), and removes
+        all of the edges whose source is not a TF designated in our TF list (created by a function such as create_TF_list).
+
+        Args:
+            db_edges = list, a list of tuples of the form (source, target, edge weight)
+            db_sign_att = list, a list of tuples of the form (source, target, sign of edge weight)
+            TF_list = list, a list of transcription factors produced from a function such as create_TF_list
+
+        Returns: edge_list_filtered = list of same form as db_edges, where only TF-as-source edges remain
+                 sign_att_list_filtered = list of same form as db_sign_att, where only TF-as-source edges remain
+
+    """
+
     # extracting TR edge information from background database
     edge_list_filtered = []
     sign_att_list_filtered = []
@@ -106,6 +178,21 @@ def filter_background(db_edges, db_sign_att, TF_list):
 
 
 def make_digraph(db_edges, db_sign_att, TF_list):
+
+    """
+        This function filters down the to given lists of edges-and-attribute by calling filter_background. This removes
+        all edge from our lists whose source node is not a transcription factor (as specified by TF_list). Our function
+        then creates a networkX directed graph fom these two edges-and-attribute lists
+
+        Args:
+            db_edges = list, a list of tuples of the form (source, target, edge weight)
+            db_sign_att = list, a list of tuples of the form (source, target, sign of edge weight)
+            TF_list = list, a list of transcription factors produced from a function such as create_TF_list
+
+        Returns: DG = DiGraph, a networkX directed graph with only TF-as-source nodes, containing attributes 'weight'
+                      and 'sign'
+    """
+
     # use only edges from background network associated with our TF list
     edge_list_filtered, sign_att_list_filtered = filter_background(db_edges, db_sign_att, TF_list)
 
@@ -119,6 +206,21 @@ def make_digraph(db_edges, db_sign_att, TF_list):
 
 
 def load_DEG_with_up_downs(filename="differencially_expressed_genes.txt", filter_value=0.3):
+
+    """
+        This function loads up a list of differentially expressed genes, removes genes with lfdr value greater
+        than the specified filter value, then extracts the up/down regulation information associated with those genes.
+
+        Args:
+            filename = string, path to our list of differencially expressed genes, containing up/down regulated information
+                       under column log2.89.12
+            filter_value = float, cutoff lfdr value to filter our DEG's by
+
+        Returns: DEG_list = list, a list of differencially expressed genes that have a lfdr value less than the filter value
+                 DEG_to_updown = dict, a dictionary that maps a DEG name to wether it is up regulated or down regulated. If
+                                 no up/down regulation info, will set value to zero
+    """
+
     # load differencially expressed genes (experimental results)
     DEG_db = pd.read_csv(filename, sep="\t")
 
@@ -135,15 +237,28 @@ def load_DEG_with_up_downs(filename="differencially_expressed_genes.txt", filter
                 DEG_list.append(str(DEG_db.symbol[i]).upper())
 
                 # creating dictionary between DEG symbols and their up/down value
-                if DEG_db['log2.89.12'][i] != 0:
-                    DEG_to_updown[str(DEG_db.symbol[i]).upper()] = DEG_db['log2.89.12'][i]
-                else:
-                    DEG_to_updown[str(DEG_db.symbol[i]).upper()] = 0
+                DEG_to_updown[str(DEG_db.symbol[i]).upper()] = DEG_db['log2.89.12'][i]
 
     return DEG_list, DEG_to_updown
 
 
 def add_updown_from_DEG(DG, DEG_filename="differencially_expressed_genes.txt", DEG_filter_value=0.3):
+
+    """
+        This function loads up a list of differentially expressed genes, removes genes with lfdr value greater
+        than the specified filter value, then extracts the up/down regulation information associated with those genes.
+        Our function then finds all DEG's located withing graph DG, and adds these up/down regulation values as a node
+        attribute.
+
+        Args:
+            DG = DiGraph, a networkX directed graph
+            DEG_filename = string, path to our list of differencially expressed genes, containing up/down regulated information
+                       under column log2.89.12
+            DEG_filter_value = float, cutoff lfdr value to filter our DEG's by
+
+        Returns: DEG_list = list, a list of differencially expressed genes that have a lfdr value less than the filter value
+    """
+
     DEG_list, DEG_to_updown = load_DEG_with_up_downs(DEG_filename, DEG_filter_value)
 
     # get all the differencially expressed genes in DG
@@ -156,3 +271,5 @@ def add_updown_from_DEG(DG, DEG_filename="differencially_expressed_genes.txt", D
     nx.set_node_attributes(DG, 'updown', zero_dict)
 
     return DEG_list
+
+
--- ura/stat_analysis.py
+++ ura/stat_analysis.py
@@ -13,14 +13,17 @@ import math
 def tr_pvalues(DG, db_edges, DEG_list):
 
     """
-        Calculates a log of a p-value for every transcription factor in the graph. These values determine
-        how statistically likely it that a specific transcription factor is randomly associated with the set
-        of differentially expressed genes found to have been associated with it in practice.
+        Our p-value function calculates the log of the p-value for every TF in the graph using [scipy.stats.hypergeom.logsf]
+        (https://docs.scipy.org/doc/scipy-0.19.1/reference/generated/scipy.stats.hypergeom.html). These values help us
+        determine which TF's are actually associated with our DEG's. If a TF is given a high value (because we are
+        working with logs, not straight p-values), then it is likely that there is correlation between that TF and its
+        DEG targets. Therefore, it is likely that TF is responsible for some of our observed gene expression.
+        Note that if a TF is given a value of zero, that means none of the TF's targets were DEG's.
 
         Args:
-            DG: A directed networkX graph with edges mapping from transcription factors to expressed genes
-            background_list: list of all genes in your experiment's universe
-            DEG_list: your list of differencially expressed genes
+            DG: Digraph, a directed networkx graph with edges mapping from transcription factors to expressed genes
+            background_list: list of strings, list of all genes in your experiment's universe
+            DEG_list: list of strings, your list of differencially expressed genes
 
         Returns: A dictionary that maps a transcription factor's gene symbol to its calculated p-vlaue log.
 
@@ -31,18 +34,37 @@ def tr_pvalues(DG, db_edges, DEG_list):
 
     TR_to_pvalue = {}
     for TR in source_nodes:
-        x = len(list(
-            set(DG.neighbors(TR)) & set(DEG_list)))  # per TR, observed overlap between TR neighbors and DEG_list
+        x = len(list(set(DG.neighbors(TR)) & set(DEG_list)))  # per TR, observed overlap between TR neighbors and DEG_list
         M = len(background_list)  # num unique nodes in universe, aka background network (STRING)
         n = len(DG.neighbors(TR))  # per TR, number of targets for that TR
         N = len(list(set(background_list) & set(DEG_list)))  # number of DEG, picked from universe "at random"
 
-        TR_to_pvalue[TR] = -(scipy.stats.hypergeom.logsf(x, M, n, N, loc=0))  # remove unnecessary negative sign
+        if x == 0:
+            TR_to_pvalue[TR] = 0;
+        else:
+            TR_to_pvalue[TR] = -(scipy.stats.hypergeom.logsf(x, M, n, N, loc=0))  # remove unnecessary negative sign
 
     return TR_to_pvalue
 
 
 def tr_zscore(DG, DEG_list):
+
+    """
+        The goal of our z-score function is to predict the activation states of the TF's. We observe how a TF relates
+        to each of its targets to make our prediction. We compare each targets' observed gene regulation (either up or
+        down) and each TF-target interaction (whether it is activating or inhibiting) to conclude whether a TF is
+        activating or inhibiting. A positive value indicates activating while a negative value indicates inhibiting.
+        A value of zero means that we did not have enough information about the target or TF-target interaction to
+        make the prediction.
+
+        Args:
+            DG: Digraph, a directed networkx graph with edges mapping from transcription factors to expressed genes
+            DEG_list: list of strings, your list of differencially expressed genes
+
+        Returns: A dictionary that maps a transcription factor's gene symbol to its calculated z-score.
+
+    """
+
     source_nodes = list(set(zip(*DG.edges())[0]))  # identifying unique source nodes in graph
 
     TR_to_zscore = {}
@@ -75,8 +97,102 @@ def tr_zscore(DG, DEG_list):
         else:
             z_score = (N_plus - N_minus) / float(math.sqrt(N))
 
-        TR_to_zscore[TR] = z_score  # create zscore dict where 1 means activating
-        # -1 means inhibiting
+        TR_to_zscore[TR] = z_score
+        # create zscore dict where positive means activating
+        # negative means inhibiting
         # 0 means could not be calculated
+
+    return TR_to_zscore
+
+
+def calculate_bias(DG, DEG_list):
+    source_nodes = list(set(zip(*DG.edges())[0]))  # identifying unique source nodes in graph
+
+    TR_to_bias = {}
+    for TR in source_nodes:
+
+        N_up = 0  # number of up regulated target
+        N_down = 0  # number of down regulated targets
+
+        N_act = 0  # number of activating edges
+        N_inh = 0  # number of inhibiting edges
+
+        N_problem = 0  # number of edges with errorous calculations
+
+        TRs_DEG_neighbors = set(DG.neighbors(TR)) & set(DEG_list)
+        for n in TRs_DEG_neighbors:
+
+            # count up edge signs
+            sign_of_edge = DG[TR][n]['sign']
+            if sign_of_edge == 1:
+                N_act += 1
+            elif sign_of_edge == -1:
+                N_inh += 1
+            else:
+                N_problem += 1
+                print "Issue with edge (" + str(TR) + ',' + str(n) + ') A/I'
+
+            # count up node regulations
+            up_down_of_n = (DG.node[n]['updown'] / abs(DG.node[n]['updown']))
+            if up_down_of_n == 1:
+                N_up += 1
+            elif up_down_of_n == -1:
+                N_down += 1
+            else:
+                N_problem += 1
+                print "Issue with edge (" + str(TR) + ',' + str(n) + ') up/down'
+
+        # calculate up down bias
+        if (N_up + N_down) != 0:
+            u_data = (N_up - N_down) / float(N_up + N_down)
+        else:
+            u_data = 0
+
+        # calculate act-inh bias
+        if (N_act + N_inh) != 0:
+            u_TR = (N_act - N_inh) / float(N_act + N_inh)
+        else:
+            u_TR = 0
+
+        # calculate overall bias
+        u = u_data * u_TR
+        TR_to_bias[TR] = u
+
+    return TR_to_bias
+
+
+def bias_corrected_tr_zscore(DG, DEG_list, TR_to_bias):
+    source_nodes = list(set(zip(*DG.edges())[0]))  # identifying unique source nodes in graph
+
+    TR_to_zscore = {}
+    for TR in source_nodes:
+        w = []
+        x = []
+        TRs_DEG_neighbors = set(DG.neighbors(TR)) & set(DEG_list)
+        for n in TRs_DEG_neighbors:
+            sign_of_edge = DG[TR][n]['sign']
+            up_down_of_n = (DG.node[n]['updown'] / abs(DG.node[n]['updown']))
+
+            # predict whether this neighbor thinks the TR is Act. or Inhib.
+            prediction = sign_of_edge * up_down_of_n
+            if ((prediction == 1) | (prediction == -1)):
+                x.append(prediction)
+            else:
+                print "Issue with edge (" + str(TR) + ',' + str(n) + ')'
+
+                # keep track of each target's weight
+            w.append(DG[TR][n]['weight'])
+
+        u = TR_to_bias[TR]
+
+        # calculate bias-corrected z-score
+        z_score_top = 0
+        z_score_bottom = 0
+        for i in range(len(w)):
+            z_score_top += w[i] * (x[i] - u)
+            z_score_bottom += w[i] * w[i]
+        z_score = z_score_top / ((z_score_bottom) ** (1 / 2))
+
+        TR_to_zscore[TR] = z_score
 
     return TR_to_zscore
